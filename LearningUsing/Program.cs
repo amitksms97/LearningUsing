36d7c5f8a596b9d05a51044c41cce696053e1d89
amitksms97 <amitksms97@gmail.com>
2021-05-01 20:38:32 +0530

    Add project files.

---
+++ LearningUsing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearningUsing
+{
+    class MyResource : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing our resource");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var resource = new MyResource())
+            {
+                Console.WriteLine("Using my resource");
+            }
+            Console.WriteLine("Finshed");
+        }
+    }
+}
